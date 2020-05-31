8ac3571c67de1d1542b2c2e11b078c35543c00d9
Bayu Adi Prasetiyo <bayu@leson.id>
2020-05-31 16:27:35 +0700

    dev plugin ngubah wording

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Laravel
+                   Latihan Git Bareng Plugin
                 </div>
 
                 <div class="links">
